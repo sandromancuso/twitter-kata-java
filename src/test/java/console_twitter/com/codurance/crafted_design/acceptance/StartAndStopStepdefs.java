9cbab5eba542c249f02038113fb4e20f7f411a61
Sandro Mancuso <sandromancuso@yahoo.com>
2014-04-15 15:32:30 +0100

    Start and stop scenario implemented

--- src/test/java/console_twitter/com/codurance/crafted_design/acceptance/StartAndStopStepdefs.java
+++ src/test/java/console_twitter/com/codurance/crafted_design/acceptance/StartAndStopStepdefs.java
@@ -1,86 +1,31 @@
 package console_twitter.com.codurance.crafted_design.acceptance;
 
+import cucumber.api.java.Before;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
-
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 public class StartAndStopStepdefs {
 
-	private TwitterConsoleTestingDSL twitterConsoleTestingDSL;
+	private TwitterConsoleTestingDSL twitterConsole;
+
+	@Before
+	public void initialise() {
+		twitterConsole = TwitterConsoleTestingDSL.start();
+	}
 
 	@Given("^the application receives an 'exit' command$")
 	public void the_application_receives_an_exit_command() throws Throwable {
-		twitterConsoleTestingDSL = TwitterConsoleTestingDSL.start();
-		twitterConsoleTestingDSL.receives("blah command");
-		twitterConsoleTestingDSL.receives("exit");
+		twitterConsole.receives("some user command");
+		twitterConsole.receives("exit");
 	}
 
 	@Then("^the application should terminate$")
 	public void the_application_should_terminate() throws Throwable {
-		String output = twitterConsoleTestingDSL.output();
+		String output = twitterConsole.output();
 		assertThat(output, is("bye!"));
-	}
-
-	static public class TwitterConsoleTestingDSL {
-
-		private List<String> userCommands = new ArrayList<>();
-
-		public static TwitterConsoleTestingDSL start() {
-			return new TwitterConsoleTestingDSL();
-		}
-
-		public void receives(String userCommand) {
-			userCommands.add(userCommand + "\n");
-		}
-
-		public String output() {
-			return runTwitterConsoleWith(userCommands);
-		}
-
-		private String runTwitterConsoleWith(List<String> userCommands) {
-			String cmd = new StringBuilder()
-					.append("/usr/bin/java -cp ./target/classes ")
-					.append("com.codurance.crafted_design.TwitterConsole")
-					.toString();
-
-			try {
-				Process p = Runtime.getRuntime().exec(cmd);
-
-				inp = new BufferedReader( new InputStreamReader(p.getInputStream()) );
-				out = new BufferedWriter( new OutputStreamWriter(p.getOutputStream()) );
-
-				PrintWriter writer = new PrintWriter( p.getOutputStream() );
-				BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
-
-				for(String userCommand: userCommands) {
-					writer.write(userCommand);
-					writer.flush();
-				}
-
-				StringBuilder output = new StringBuilder();
-				String outputLine = "";
-				while ((outputLine = reader.readLine()) != null) {
-					output.append(outputLine);
-				}
-
-
-				p.waitFor();
-				return output.toString();
-
-			} catch (Exception e) {
-				throw new RuntimeException(e);
-			}
-		}
-
-		public static BufferedReader inp;
-		public static BufferedWriter out;
-
 	}
 
 }
---
+++ src/test/java/console_twitter/com/codurance/crafted_design/acceptance/TwitterConsoleTestingDSL.java
@@ -0,0 +1,71 @@
+package console_twitter.com.codurance.crafted_design.acceptance;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TwitterConsoleTestingDSL {
+
+	private List<String> userCommands = new ArrayList<>();
+
+	public static TwitterConsoleTestingDSL start() {
+		return new TwitterConsoleTestingDSL();
+	}
+
+	public void receives(String userCommand) {
+		userCommands.add(userCommand + "\n");
+	}
+
+	public String output() {
+		try {
+			return runTwitterConsoleWith(userCommands);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private String runTwitterConsoleWith(List<String> userCommands) throws IOException {
+		Process process = execute(TWITTER_CONSOLE_ON_COMMAND_LINE);
+
+		sendUserCommandsToProcess(userCommands, process);
+
+		return readOutputFrom(process);
+	}
+
+	private String readOutputFrom(Process process) throws IOException {
+		processReader = reader(process);
+		String outputLine;
+		StringBuilder output = new StringBuilder();
+		while ((outputLine = processReader.readLine()) != null) {
+			output.append(outputLine);
+		}
+		return output.toString();
+	}
+
+	private void sendUserCommandsToProcess(List<String> userCommands, Process process) {
+		PrintWriter processWriter = writer(process);
+		for(String userCommand: userCommands) {
+			processWriter.write(userCommand);
+			processWriter.flush();
+		}
+	}
+
+	private PrintWriter writer(Process process) {
+		return new PrintWriter( process.getOutputStream() );
+	}
+
+	private BufferedReader reader(Process process) {
+		return new BufferedReader( new InputStreamReader(process.getInputStream()) );
+	}
+
+	private Process execute(String command) throws IOException {
+		return Runtime.getRuntime().exec(command);
+	}
+
+	public static BufferedReader processReader;
+
+	private static String TWITTER_CONSOLE_ON_COMMAND_LINE =
+							"/usr/bin/java -cp ./target/classes " +
+							"com.codurance.crafted_design.TwitterConsole";
+
+}
